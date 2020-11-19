059bcf56fd392b915366ff65bf2d5a42151a3a7c
VKuzia <v.kuzia.mail@gmail.com>
2020-11-20 01:39:51 +0300

    Make unit-test url final
    Теперь тестовый url является полем HttpPostUnitTest

--- app/src/test/java/HttpPostUnitTest.java
+++ app/src/test/java/HttpPostUnitTest.java
@@ -9,11 +9,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class HttpPostUnitTest {
+    private final String url = "http://maxim12321-test.herokuapp.com";
 
     @Test
     public void httpClient_correctGetPost() {
         UserInfo userInfo = new UserInfo(0);
-        String url = "http://maxim12321-test.herokuapp.com";
         HTTPClient httpClient = new HTTPClient(userInfo, url);
         HashMap<String, String> loginData = new HashMap<String, String>() {{
             put("username", "Kuzia");
