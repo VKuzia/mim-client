f299534b457a216abbebb74cbe9adb7b6bd19dda
maxim12321 <31935266+maxim12321@users.noreply.github.com>
2020-12-01 13:05:30 +0300

    Add UI for creating new chats (#5)
    Добавлен интерфейс для создания нового чата.
    При нажатии на кнопку "+" (добавить чат), открывается окно создания чата. Туда вводится
    имя создаваемого чата, после чего чат добавляется в список чатов.
    Вместо аватарки чата теперь отображается кружок с первым символом из названия.

---
+++ app/src/main/java/com/mimteam/mimclient/activities/CreateChatActivity.java
@@ -0,0 +1,80 @@
+package com.mimteam.mimclient.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.mimteam.mimclient.R;
+import com.mimteam.mimclient.components.ChatAvatar;
+
+public class CreateChatActivity extends AppCompatActivity {
+
+    private EditText chatNameEdit;
+    private Button createChatButton;
+    private ChatAvatar chatAvatar;
+    private Toolbar createChatToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_chat_activity);
+
+        initializeUIComponents();
+        setSupportActionBar(createChatToolbar);
+        attachListenersToComponents();
+    }
+
+    private void initializeUIComponents() {
+        chatNameEdit = findViewById(R.id.createChatName);
+        createChatButton = findViewById(R.id.createChatButton);
+        chatAvatar = findViewById(R.id.createChatAvatar);
+        createChatToolbar = findViewById(R.id.toolBarCreateChat);
+    }
+
+    private void attachListenersToComponents() {
+        createChatButton.setOnClickListener(view -> createChat());
+        createChatToolbar.setNavigationOnClickListener(view -> openChatListActivity());
+        chatNameEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (chatAvatar.setChatName(chatNameEdit.getText().toString())) {
+                    setChatNameError();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void openChatListActivity() {
+        Intent intent = new Intent(CreateChatActivity.this, ChatListActivity.class);
+        startActivity(intent);
+    }
+
+    private void setChatNameError() {
+        chatNameEdit.requestFocus();
+        chatNameEdit.setError(getString(R.string.chat_name_error));
+    }
+
+    private void createChat() {
+        if (chatNameEdit.getError() != null || chatNameEdit.getText().length() == 0) {
+            setChatNameError();
+            return;
+        }
+        Intent intent = new Intent(CreateChatActivity.this, ChatListActivity.class);
+        intent.putExtra(getString(R.string.chat_name_variable), chatNameEdit.getText().toString());
+        startActivity(intent);
+    }
+}
