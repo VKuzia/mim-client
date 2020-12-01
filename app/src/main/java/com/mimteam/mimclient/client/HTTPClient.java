958d1d4ada47af189dc31f6828e8131984de653c
Vladimir Kuzmitski <44345886+VKuzia@users.noreply.github.com>
2020-12-02 01:00:10 +0300

    Add HTTP Client (#4)
    Добавлен класс HTTPClient, осуществляющий GET/POST запросы с использованием okHTTP.
    Добавлена обёртка HTTPWrapper, предоставляющая удобный интерфейс для осуществления операций запроса.
    Добавлена зависимость Google Guava для использования удобных Optional и реализаций Map.
    Добавлена зависимость Jackson для парсинга ответов от сервера в необходимый тип.
    Добавлено ненулевое содержимое файла proguard-rules.pro для урезания jar файла (неиспользуемых классов).

---
+++ app/src/main/java/com/mimteam/mimclient/client/HTTPClient.java
@@ -0,0 +1,102 @@
+package com.mimteam.mimclient.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class HTTPClient {
+    private final String url;
+    private final UserInfo userInfo;
+    private final OkHttpClient okHttpClient;
+
+    public HTTPClient(UserInfo userInfo, String url) {
+        this.userInfo = userInfo;
+        this.url = url;
+        this.okHttpClient = new OkHttpClient();
+    }
+
+    public Optional<String> get(String urlSuffix) {
+        return get(urlSuffix, ImmutableMap.of());
+    }
+
+    public Optional<String> get(String urlSuffix, Map<String, String> params) {
+        Request request = formGetRequest(urlSuffix, params);
+        return sendRequest(request);
+    }
+
+    public Optional<String> post(String urlSuffix, Map<String, String> params) {
+        Request request = formPostRequest(urlSuffix, params);
+        return sendRequest(request);
+    }
+
+    private Request formGetRequest(String urlSuffix, @NotNull Map<String, String> params) {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url + urlSuffix).newBuilder();
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            urlBuilder.addQueryParameter(param.getKey(), param.getValue());
+        }
+        Headers headers = createHeaders();
+        return new Request.Builder()
+                .url(urlBuilder.build().url().toString())
+                .headers(headers)
+                .get()
+                .build();
+    }
+
+    private Request formPostRequest(String urlSuffix, @NotNull Map<String, String> params) {
+        FormEncodingBuilder formBody = new FormEncodingBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            formBody.add(entry.getKey(), entry.getValue());
+        }
+        Headers headers = createHeaders();
+        return new Request.Builder()
+                .url(url + urlSuffix)
+                .headers(headers)
+                .post(formBody.build())
+                .build();
+    }
+
+    private @NotNull Headers createHeaders() {
+        return Headers.of("Authorization", "Bearer " + userInfo.getToken());
+    }
+
+    private @Nullable Optional<String> sendRequest(Request request) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<String> callable = () -> {
+            Response response = okHttpClient.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException(response + ": " + response.body().string());
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            ResponseDTO responseDTO = mapper.readValue(response.body().string(), ResponseDTO.class);
+            return responseDTO.getResponseMessage();
+        };
+        Future<String> future = executor.submit(callable);
+        try {
+            return Optional.fromNullable(future.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Optional.absent();
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+}
